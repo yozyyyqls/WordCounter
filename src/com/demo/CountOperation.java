f097cccf51ae3104effd1530751c42729aca058c
yozyyyqls <yozyyyqls@163.com>
2020-03-17 18:28:36 +0800

    WordCounter 第一次提交
    完成字符/单词/行数的计算；完成文件批处理；单词数计算有待优化

---
+++ src/com/demo/CountOperation.java
@@ -0,0 +1,5 @@
+package com.demo;
+
+public interface CountOperation {
+    int op(byte[] fileByte);
+}
---
+++ src/com/demo/CountType.java
@@ -0,0 +1,33 @@
+package com.demo;
+
+public enum CountType implements CountOperation {
+    OP_C{
+        @Override
+        public int op(byte[] fileByte) {
+            if(fileByte==null) return 0;    //文件为空
+            String fileStr = new String(fileByte);
+            String pattern = "\\S";
+            return Tools.searchStr(pattern, fileStr);
+        }
+    },
+
+    OP_W{
+        @Override
+        public int op(byte[] fileByte){
+            if(fileByte==null) return 0;
+            String fileStr = new String(fileByte);
+            String REGEX = "[a-zA-Z]{2,}\\W";
+            return Tools.searchStr(REGEX, fileStr);
+        }
+    },
+
+    OP_L{
+        @Override
+        public int op(byte[] fileByte){
+            if(fileByte==null) return 0;
+            String REGEX = "\\n";
+            String fileStr = new String(fileByte);
+            return Tools.searchStr(REGEX, fileStr)+1;//最后一行通常无回车
+        }
+    };
+}
